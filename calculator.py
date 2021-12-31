16b619e6adfcbf058491cbddc3067a02809e6650
DustinThebegginer <79025357+DustinThebegginer@users.noreply.github.com>
2021-12-31 14:58:59 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+num1 = float(input("num1: "))
+x = input("*,/,-,+ : ")
+num2 = float(input("num2: "))
+
+if x == "*":
+  print(num1 * num2)
+elif x == "/":
+  print(num1 / num2)
+elif x == "-":
+  print(num1 - num2)
+elif x == "+":
+  print(num1 + num2)
+else:
+  print("error")  
+
+
+
+
+    
+    
